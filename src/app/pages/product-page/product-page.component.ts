e7103f64d404ccd94da99b524a1556ac541173a4
ibaraness <idan.baraness@gmail.com>
2017-05-20 11:33:57 +0300

    Added show all reviews in product page

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -13,6 +13,8 @@ export class ProductPageComponent implements OnInit {
 
   private product: IProduct;
   private reviews: IReview[];
+  private showingAllReviews: boolean;
+  private hasMoreThanThreeReviews = false;
 
   constructor(private route: ActivatedRoute, private reviewsService: ReviewsService) { }
 
@@ -26,15 +28,32 @@ export class ProductPageComponent implements OnInit {
       /**
        * Get 3 first reviews for that specific product
        */
+      this.showFirstThreeReviews();
+    })
+  }
+
+  /**
+   * Get 3 first reviews for that specific product
+   */
+  private showFirstThreeReviews(): void {
       if(this.product){     
         this.reviews = this.reviewsService.getAll(this.product.id);
         if(this.reviews && this.reviews.length > 3){
           this.reviews = this.reviews.slice(0,3);
-        }
+          this.hasMoreThanThreeReviews = true;
+          this.showingAllReviews = false;
+        } 
       }
-    })
+  }
 
-    
+  /**
+   * Get All reviews for that specific product
+   */
+  private showAllReviews(): void {
+      if(this.product){     
+        this.reviews = this.reviewsService.getAll(this.product.id);
+        this.showingAllReviews = true;
+      }
   }
 
 }
